41d547d0ff53d606db8674103ab3d136d7a53c7f
PRASHANT GUPTA <87434010+Prashant844@users.noreply.github.com>
2023-01-10 21:00:06 +0530

    Add files via upload

---
+++ FindSumUsingLoop.c
@@ -0,0 +1,45 @@
+//Write a program to getting the Sum of number from 1 to 100;
+
+#include <stdio.h>
+
+void main()
+{
+           printf("\n********** Start Program *************\n");
+           
+           printf("\n****** Using For Loop ******\n");
+           int x =1 , Sum1 = 0;
+           for(; x<=100; x++)
+           {
+               Sum1 += x;
+           }
+           printf("THE SUM OF 1 TO 100 IS : %d\n",Sum1);
+           
+        //   printf("\n****** Using while Loop ******\n");
+           
+        //   int y = 1 , Sum2 = 0;
+        //   while(y <= 100)
+        //   {
+        //        Sum2 += y;
+        //        y++;
+        //   }
+        //    printf("THE SUM OF 1 TO 100 IS : %d\n",Sum2);
+           
+          // printf("\n****** Using do-while Loop ******\n");
+          // int z = 1 ,Sum3 = 0;
+          //  do
+          //  {
+          //      Sum3 += z;
+          //      z++;
+          //  }
+          //  while(z <= 100);
+          //  printf("THE SUM OF 1 TO 100 IS : %d\n",Sum3);
+           
+          // printf("\n********** End Program *************\n");
+
+          // printf("Compiled By : PRASHANT GUPTA  L-ID :2224MCA1004");
+          
+          
+        
+
+
+}
