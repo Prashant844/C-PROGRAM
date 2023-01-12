9b651af9a19e531d35171267268965f9b1aa66ae
PRASHANT GUPTA <87434010+Prashant844@users.noreply.github.com>
2023-01-12 19:15:29 +0530

    Add files via upload

---
+++ NumberPrintUsingLoop.c
@@ -0,0 +1,40 @@
+//write a program , print number from 1 to 10 using following looping statement.
+//1. for loop
+//2. while loop
+//3. do-while loop
+
+#include <stdio.h>
+
+void main()
+{
+           printf("\n********** Start Program *************\n");
+           
+        //    printf("\n****** Using For Loop ******\n");
+        //    int x = 1;
+        //    for( ; x <= 10; x++)
+        //    {
+        //       printf(" X is : %d\n",x);
+        //    }
+           
+           
+        //    printf("\n****** Using while Loop ******\n");
+           
+        //    int y = 1;
+        //    while(y <= 10)
+        //    {
+        //        printf(" Y is : %d\n",y);
+        //        y++;
+        //    }
+           
+           printf("\n****** Using do-while Loop ******\n");
+           int z = 1;
+           do
+           {
+              printf(" Z is : %d\n",z);
+              z++;
+           }
+           while(z <= 10);
+           
+           printf("\n********** End Program *************\n");
+          printf("Compiled By : PRASHANT GUPTA L-ID : 2224MCA1004");
+}
