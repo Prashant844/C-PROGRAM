480f4ea593a75fe4283c12ab8a25e0279e499787
PRASHANT GUPTA <87434010+Prashant844@users.noreply.github.com>
2023-01-18 23:13:04 +0530

    Add files via upload

---
+++ TablePrintUsingLoop.c
@@ -0,0 +1,19 @@
+//Write a program to print the table from 1 to 10 using nested loop.
+#include <stdio.h>
+
+void main()
+{
+           printf("\n********** Start Program *************\n");
+           
+          for(int i=1; i<=1; i++)
+          {
+              printf("\n********* Table of %d *********\n",i);
+             for(int j = 1; j<=10; j++)
+             {
+             printf("\n %d * %d = %d",i,j,i*j);
+             }
+          }
+          printf("\n\n********** End Program ***************\n");
+          printf("\n Compiled By : PRASHANT GUPTA L-ID : 2224MCA1004");
+          
+}
