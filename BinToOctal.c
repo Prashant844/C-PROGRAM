1c3eaf0f4ce0c4520b5432881d9bd33af5c9fafa
PRASHANT GUPTA <87434010+Prashant844@users.noreply.github.com>
2023-03-13 21:08:00 +0530

    Add files via upload

---
+++ BinToOctal.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    int binary, Decimal = 0, weight=1;
+    printf("Enter a Binary Number here ,only these two digit contain(0,1): ");
+    scanf("%d",&binary);
+    while(binary != 0)
+    {
+        int digit = binary % 10;
+        Decimal = Decimal + digit * weight;
+        binary = binary / 10;
+        weight = weight * 8;
+    }
+    printf("Decimal Equaivalent is : %d ",Decimal);
+}
